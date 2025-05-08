ccac2e6f247284f2d919e4a5e091485107a8d53a
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-08 11:45:07 -0300

    Container FrmPrincipal, Windows State maximazed.

--- ComercialTDSDesk/FrmPrincipal.Designer.cs
+++ ComercialTDSDesk/FrmPrincipal.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmPrincipal";
+            SuspendLayout();
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            IsMdiContainer = true;
+            Name = "FrmPrincipal";
+            Text = "Sistema Comercial TDS T01";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
         }
 
         #endregion
