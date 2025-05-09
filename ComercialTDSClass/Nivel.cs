b052a928738ed57331f6ef6955544892b90dc24d
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-09 09:19:51 -0300

    Propriedades adicionada na classe Nivel

--- ComercialTDSClass/Nivel.cs
+++ ComercialTDSClass/Nivel.cs
@@ -8,5 +8,28 @@ namespace ComercialTDSClass
 {
     class Nivel
     {
+
+        public int? Id { get; set; }
+        public string? Nome { get; set; }
+        public string Sigla { get; set; }
+
+        public Nivel()
+        {
+
+        }
+        public Nivel(int id, string nome, string sigla)
+        {
+            Id = id;
+            Nome = nome;
+            Sigla = sigla;
+        }
+
+        public Nivel( string nome, string sigla)
+        {
+            Nome = nome;
+            Sigla = sigla;
+        }
+
+        // Inserir, Atualizar
     }
 }
