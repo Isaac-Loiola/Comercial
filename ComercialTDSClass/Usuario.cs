898aad9c26592d9975bf1136ad533cc8a8a79e0b
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-12 09:08:35 -0300

    Método inserir adicionado na classe Usuario.

--- ComercialTDSClass/Usuario.cs
+++ ComercialTDSClass/Usuario.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace ComercialTDSClass
 {
@@ -14,13 +15,13 @@ namespace ComercialTDSClass
         public string? Nome { get; set; }
         public string? Email { get; set; }
         public string? Senha { get; set; }
-        public Nivel? NivelId { get; set; }
+        public Nivel? Nivel { get; set; }
         public bool? Ativo { get; set; }
         
         // Métodos construtores.
         public Usuario()
         {
-             
+            Nivel = new();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
         }
         // Receber todos os dados
         public Usuario(int? id, string? nome, string? email, string? senha, Nivel? nivelId, bool? ativo)
@@ -29,7 +30,7 @@ namespace ComercialTDSClass
             Nome = nome;
             Email = email;
             Senha = senha;
-            NivelId = nivelId;
+            Nivel = nivelId;
             Ativo = ativo;
         }
 
@@ -38,7 +39,7 @@ namespace ComercialTDSClass
             Nome = nome;
             Email = email;
             Senha = senha;
-            NivelId = nivelId;
+            Nivel = nivelId;
         }
 
         // Insert
@@ -47,7 +48,7 @@ namespace ComercialTDSClass
             Nome = nome;
             Email = email;
             Senha = senha;
-            NivelId = nivelId;
+            Nivel = nivelId;
             Ativo = ativo;
         }
 
@@ -62,13 +63,24 @@ namespace ComercialTDSClass
             Id = id;
             Nome = nome;
             Senha = senha;
-            NivelId = nivelId;
+            Nivel = nivelId;
             Ativo = ativo;
         }
 
         public void Inserir()
         {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
 
+            cmd.CommandText = "sp_usuario_insert";
+            cmd.Parameters.AddWithValue("spnome", Id);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.Parameters.AddWithValue("spsenha", Senha);
+            cmd.Parameters.AddWithValue("spnivel", Nivel.Id);
+
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+            cmd.Connection.Close();
         }
 
         public bool Atualizar()
