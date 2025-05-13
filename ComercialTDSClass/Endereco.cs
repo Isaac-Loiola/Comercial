884d99475eda33c3b97016f0796ed5d7e4a59940
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-13 09:10:18 -0300

    Método ObterListaPorIdClienteId adicionado na classe Endereco.

--- ComercialTDSClass/Endereco.cs
+++ ComercialTDSClass/Endereco.cs
@@ -10,8 +10,10 @@ namespace ComercialTDSClass
 {
     public class Endereco
     {
+
         public int? Id { get; set;}
         public int? ClienteId { get; set; }
+        public string? Cep { get; set; }
         public string? Logradouro { get; set; }
         public string? Numero { get; set; }
         public string? Complemento { get; set; }
@@ -19,6 +21,32 @@ namespace ComercialTDSClass
         public string? Cidade { get; set;}
         public string? Uf { get; set; }
         public string? TipoEndereco { get; set; }
+
+        public Endereco(int? id, int? clienteId, string cep, string? logradouro, string? numero, string? complemento, string? bairro, string? cidade, string? uf, string? tipoEndereco)
+        {
+            Id = id;
+            ClienteId = clienteId;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            TipoEndereco = tipoEndereco;
+        }
+
+        public Endereco(int? clienteId, string cep, string? logradouro, string? numero, string? complemento, string? bairro, string? cidade, string? uf, string? tipoEndereco)
+        {
+            ClienteId = clienteId;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            TipoEndereco = tipoEndereco;
+        }
 
         public static List<Endereco> ObterListaPorClienteId(int idCliente)
         {
@@ -29,7 +57,21 @@ namespace ComercialTDSClass
             var dr = cmd.ExecuteReader();
             while (dr.Read())
             {
-
+                enderecos.Add
+                    (
+                        new
+                            (
+                                dr.GetInt32(1),
+                                dr.GetString(2),
+                                dr.GetString(3),
+                                dr.GetString(4),
+                                dr.GetString(5),
+                                dr.GetString(6),
+                                dr.GetString(7),
+                                dr.GetString(8),
+                                dr.GetString(9)
+                            )
+                    );
             }
 
             return enderecos;
