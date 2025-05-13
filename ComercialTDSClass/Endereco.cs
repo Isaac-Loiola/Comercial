5a06a4cc936f25a020561d4902b6d55b6814309d
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-13 09:15:14 -0300

    Método Atualizar adicionado na classe Endereco.

--- ComercialTDSClass/Endereco.cs
+++ ComercialTDSClass/Endereco.cs
@@ -67,6 +67,23 @@ namespace ComercialTDSClass
             cmd.Connection.Close();
         }
 
+        public bool Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_endereco_update";
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.Parameters.AddWithValue("spcep", Cep);
+            cmd.Parameters.AddWithValue("splogradouro", Logradouro);
+            cmd.Parameters.AddWithValue("spnumero", Numero);
+            cmd.Parameters.AddWithValue("spcomplemento", Complemento);
+            cmd.Parameters.AddWithValue("spbairro", Bairro);
+            cmd.Parameters.AddWithValue("spcidade", Cidade);
+            cmd.Parameters.AddWithValue("spuf", Uf);
+            cmd.Parameters.AddWithValue("sptipo_endereco", TipoEndereco);
+            return cmd.ExecuteNonQuery() > 0 ? true : false;
+        }
+
         public static List<Endereco> ObterListaPorClienteId(int idCliente)
         {
             List<Endereco> enderecos = new();
