bbec5c5760142f44ab6b2456554e2ff23c5907da
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-13 09:04:34 -0300

    Propriedades da classe Endereco adicionado!

--- ComercialTDSClass/Endereco.cs
+++ ComercialTDSClass/Endereco.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
 
 namespace ComercialTDSClass
 {
     public class Endereco
     {
+        public int? Id { get; set;}
+        public int? ClienteId { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set;}
+        public string? Uf { get; set; }
+        public string? TipoEndereco { get; set; }
+
         public static List<Endereco> ObterListaPorClienteId(int idCliente)
         {
             List<Endereco> enderecos = new();
-                
+
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from enderecos where id = {idCliente}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+
+            }
 
             return enderecos;
         } 
