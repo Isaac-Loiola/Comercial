2663ae6ae6e7511269af4375198bedca58e3f00f
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-13 09:14:36 -0300

    Método Inserir adicionado na classe Endereco.

--- ComercialTDSClass/Endereco.cs
+++ ComercialTDSClass/Endereco.cs
@@ -48,6 +48,25 @@ namespace ComercialTDSClass
             TipoEndereco = tipoEndereco;
         }
 
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_endereco_insert";
+            cmd.Parameters.AddWithValue("spcliente_id", ClienteId);
+            cmd.Parameters.AddWithValue("spcep", Cep);
+            cmd.Parameters.AddWithValue("splogradouro", Logradouro);
+            cmd.Parameters.AddWithValue("spnumero", Numero);
+            cmd.Parameters.AddWithValue("spcomplemento", Complemento);
+            cmd.Parameters.AddWithValue("spbairro", Bairro);
+            cmd.Parameters.AddWithValue("spcidade", Cidade);
+            cmd.Parameters.AddWithValue("spuf", Uf);
+            cmd.Parameters.AddWithValue("sptipo_endereco", TipoEndereco);
+
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+            cmd.Connection.Close();
+        }
+
         public static List<Endereco> ObterListaPorClienteId(int idCliente)
         {
             List<Endereco> enderecos = new();
