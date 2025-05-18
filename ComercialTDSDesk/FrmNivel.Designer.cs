206f9b074e9e696651bf2ae2e255543176773220
Isaac Loiola <isaackopi@icloud.com>
2025-05-18 17:18:26 -0300

    Botões FrmNivel alterados, pnlAdicionarNivel redimensionado!

--- ComercialTDSDesk/FrmNivel.Designer.cs
+++ ComercialTDSDesk/FrmNivel.Designer.cs
@@ -50,53 +50,68 @@
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(515, 427);
+            btnCancelar.BackColor = Color.Transparent;
+            btnCancelar.BackgroundImage = (Image)resources.GetObject("btnCancelar.BackgroundImage");
+            btnCancelar.FlatAppearance.BorderSize = 0;
+            btnCancelar.FlatStyle = FlatStyle.Flat;
+            btnCancelar.ForeColor = Color.White;
+            btnCancelar.Location = new Point(486, 375);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.Size = new Size(102, 50);
             btnCancelar.TabIndex = 0;
             btnCancelar.Text = "&Cancelar";
-            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.UseVisualStyleBackColor = false;
             btnCancelar.Click += btnCancelar_Click;
             // 
             // btnGravar
             // 
-            btnGravar.Location = new Point(198, 427);
+            btnGravar.BackColor = Color.Transparent;
+            btnGravar.BackgroundImage = (Image)resources.GetObject("btnGravar.BackgroundImage");
+            btnGravar.FlatAppearance.BorderSize = 0;
+            btnGravar.FlatStyle = FlatStyle.Flat;
+            btnGravar.ForeColor = Color.White;
+            btnGravar.Location = new Point(196, 375);
             btnGravar.Name = "btnGravar";
-            btnGravar.Size = new Size(75, 23);
+            btnGravar.Size = new Size(102, 50);
             btnGravar.TabIndex = 1;
             btnGravar.Text = "&Gravar";
-            btnGravar.UseVisualStyleBackColor = true;
+            btnGravar.UseVisualStyleBackColor = false;
             btnGravar.Click += btnGravar_Click;
             // 
             // btnEditar
             // 
+            btnEditar.BackColor = Color.Transparent;
+            btnEditar.BackgroundImage = (Image)resources.GetObject("btnEditar.BackgroundImage");
             btnEditar.Enabled = false;
-            btnEditar.Location = new Point(365, 427);
+            btnEditar.FlatAppearance.BorderSize = 0;
+            btnEditar.FlatStyle = FlatStyle.Flat;
+            btnEditar.ForeColor = Color.White;
+            btnEditar.Location = new Point(339, 375);
             btnEditar.Name = "btnEditar";
-            btnEditar.Size = new Size(75, 23);
+            btnEditar.Size = new Size(102, 50);
             btnEditar.TabIndex = 2;
             btnEditar.Text = "&Editar";
-            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.UseVisualStyleBackColor = false;
             btnEditar.Click += btnEditar_Click;
             // 
             // txtNome
             // 
-            txtNome.Location = new Point(70, 97);
+            txtNome.Location = new Point(78, 61);
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(237, 23);
             txtNome.TabIndex = 3;
             // 
             // txtId
             // 
-            txtId.Location = new Point(70, 18);
+            txtId.Location = new Point(265, 20);
             txtId.Name = "txtId";
-            txtId.Size = new Size(100, 23);
+            txtId.Size = new Size(50, 23);
             txtId.TabIndex = 4;
             txtId.Visible = false;
             // 
             // txtSigla
             // 
-            txtSigla.Location = new Point(70, 161);
+            txtSigla.Location = new Point(78, 134);
             txtSigla.Name = "txtSigla";
             txtSigla.Size = new Size(100, 23);
             txtSigla.TabIndex = 5;
@@ -148,7 +163,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(70, 79);
+            label1.Location = new Point(78, 43);
             label1.Name = "label1";
             label1.Size = new Size(40, 15);
             label1.TabIndex = 7;
@@ -157,7 +172,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(70, 143);
+            label2.Location = new Point(78, 116);
             label2.Name = "label2";
             label2.Size = new Size(32, 15);
             label2.TabIndex = 8;
@@ -182,9 +197,9 @@
             pnlAdicionarNivel.Controls.Add(txtSigla);
             pnlAdicionarNivel.Controls.Add(txtId);
             pnlAdicionarNivel.Controls.Add(txtNome);
-            pnlAdicionarNivel.Location = new Point(198, 144);
+            pnlAdicionarNivel.Location = new Point(196, 180);
             pnlAdicionarNivel.Name = "pnlAdicionarNivel";
-            pnlAdicionarNivel.Size = new Size(392, 277);
+            pnlAdicionarNivel.Size = new Size(392, 188);
             pnlAdicionarNivel.TabIndex = 10;
             // 
             // FrmNivel
--- ComercialTDSDesk/obj/Debug/net8.0-windows/ComercialTDSDesk.AssemblyInfo.cs
+++ ComercialTDSDesk/obj/Debug/net8.0-windows/ComercialTDSDesk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComercialTDSDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea350b2036b38ec9cd10b659f19f76e95fb1e751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09956c0c8e36a805e0320532c9efaa5c753d18b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComercialTDSDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComercialTDSDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
