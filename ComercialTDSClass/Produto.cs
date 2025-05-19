6cf054ec79e0533d242abbd916858cc4738bc2c8
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-19 09:17:30 -0300

    Adicionando métodos construtores.

--- ComercialTDSClass/Produto.cs
+++ ComercialTDSClass/Produto.cs
@@ -9,6 +9,7 @@ namespace ComercialTDSClass
 {
     public class Produto
     {
+
         public int? Id { get; set; }
         public string? CodBarras { get; set; }
         public string? Descricao { get; set; }
@@ -20,6 +21,39 @@ namespace ComercialTDSClass
         public byte[]? Imagem { get; set; }
         public DateTime DataCad { get; set; }
         public int? Descontinuado { get; set; }
+
+        // Métodos construtores
+        public Produto()
+        {
+            Categoria = new();
+        }
+
+        public Produto(int? id, string? codBarras, string? descricao, double valorUnit, string? unidadeVenda, Categoria? categoria, double estoqueMinimo, double desconto, byte[]? imagem, DateTime dataCad, int? descontinuado)
+        {
+            Id = id;
+            CodBarras = codBarras;
+            Descricao = descricao;
+            ValorUnit = valorUnit;
+            UnidadeVenda = unidadeVenda;
+            Categoria = categoria;
+            EstoqueMinimo = estoqueMinimo;
+            Desconto = desconto;
+            Imagem = imagem;
+            DataCad = dataCad;
+            Descontinuado = descontinuado;
+        }
+
+        public Produto(string? codBarras, string? descricao, double valorUnit, string? unidadeVenda, Categoria? categoria, double estoqueMinimo, double desconto)
+        {
+            CodBarras = codBarras;
+            Descricao = descricao;
+            ValorUnit = valorUnit;
+            UnidadeVenda = unidadeVenda;
+            Categoria = categoria;
+            EstoqueMinimo = estoqueMinimo;
+            Desconto = desconto;
+        }
+
 
     }
 }
