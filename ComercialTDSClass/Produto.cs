caeda785d7341d4a0592cb87453cf61275f54cd8
Isaac Oliveira Loiola <isaackopi@icloud.com>
2025-05-19 09:04:05 -0300

    Propriedades adicionadas na classe Produto.

--- ComercialTDSClass/Produto.cs
+++ ComercialTDSClass/Produto.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComercialTDSClass
 {
-    class Produto
+    public class Produto
     {
+        public int? Id { get; set; }
+        public string? CodBarras { get; set; }
+        public string? Descricao { get; set; }
+        public double ValorUnit { get; set; }
+        public string? UnidadeVenda { get; set; }
+        public Categoria? Categoria { get; set; }
+        public double EstoqueMinimo { get; set; }
+        public  double Desconto { get; set; }
+        public byte[]? Imagem { get; set; }
+        public DateTime DataCad { get; set; }
+        public int? Descontinuado { get; set; }
 
     }
 }
