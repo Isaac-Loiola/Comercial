c254fde90b529cd85433c7063b98e191144a5f3a
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-05-21 11:51:08 -0300

    Propriedades adicionados a classe Fornecedor.

--- ComercialTDSClass/Fornecedor.cs
+++ ComercialTDSClass/Fornecedor.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ComercialTDSClass
 {
-    class Fornecedor
+    public class Fornecedor
     {
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Fantasia { get; set; }
+        public string Cnpj { get; set; }
+        public string Contato { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
     }
 }
