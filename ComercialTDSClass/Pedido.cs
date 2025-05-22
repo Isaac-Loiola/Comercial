9879096bb3f3502c7af766b3817abfbf04b0aabb
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-05-22 08:52:02 -0300

    Propriedades adicionadas a classe Pedidio.

--- ComercialTDSClass/Pedido.cs
+++ ComercialTDSClass/Pedido.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ComercialTDSClass
 {
-    class Pedido
+    public class Pedido
     {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Cliente Cliente { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+        public double Desconto { get; set; }
     }
 }
