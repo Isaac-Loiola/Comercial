519fb63cfde1c316e1e57802b1efd0f6135e79a5
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-05-28 09:23:35 -0300

    Funcionalidade de inserir cliente adicionado!!

--- ComercialTDSDesk/FrmCliente.cs
+++ ComercialTDSDesk/FrmCliente.cs
@@ -25,6 +25,27 @@ namespace ComercialTDSDesk
 
         private void btnInserir_Click(object sender, EventArgs e)
         {
+            txtCpf.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+            txtTelefone.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+
+            if (txtNome.Text != string.Empty && txtCpf.Text != string.Empty && txtTelefone.Text != string.Empty && txtEmail.Text != string.Empty && txtDataNascimento.Text != string.Empty)
+            {
+                Cliente cliente = new(txtNome.Text, txtCpf.Text, txtTelefone.Text, txtEmail.Text, Convert.ToDateTime(txtDataNascimento.Text));
+                cliente.Inserir();
+                if(cliente.Id > 0)
+                {
+                    MessageBox.Show($"{cliente.Nome} inserido com sucesso!");
+                }
+                else
+                {
+                    MessageBox.Show($"Falha ao inserir {cliente.Nome}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Todos campos são obrigatórios para inserir!");
+            }
+            
             
         }
     }
--- ComercialTDSDesk/obj/Debug/net8.0-windows/ComercialTDSDesk.AssemblyInfo.cs
+++ ComercialTDSDesk/obj/Debug/net8.0-windows/ComercialTDSDesk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComercialTDSDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8adde53544c5faae4489c3f993ac3f38a63e46a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2d5ea3689abecc3a62c799edbaa567936cd2c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComercialTDSDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComercialTDSDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
